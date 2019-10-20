83ad3fa83ba15cd63aa697d83a0843f4a75385c9
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2019-10-20 22:06:39 +0200

    #1: Removed initial props from ErrorPage
    Due to the serverless static export, we won't get access to res.statusCode initial props.

--- pages/_error.js
+++ pages/_error.js
@@ -1,14 +1,9 @@
-import t from "prop-types";
 import React from "react";
 
 const ErrorPage = props => {
-  const { statusCode } = props;
-
   return (
     <div className="splash-page flex flex-col flex-grow justify-center items-center">
-      <p className="mx-8 text-center text-3xl text-mariner font-bold">
-        {statusCode}
-      </p>
+      <p className="mx-8 text-center text-3xl text-mariner font-bold">404</p>
 
       <p className="mx-8 mt-8 text-center text-xl text-gray-700">
         Well, well, well. What do we have here? Someone trying to sneak into
@@ -22,23 +17,6 @@ const ErrorPage = props => {
       <p className="mx-8 mt-8 text-center text-xl text-gray-700">Thanks.</p>
     </div>
   );
-};
-
-ErrorPage.propTypes = {
-  statusCode: t.number
-};
-
-ErrorPage.getInitialProps = ({ err, res }) => {
-  let code = null;
-  if (res) {
-    const { statusCode } = res;
-    code = statusCode;
-  } else if (err) {
-    const { statusCode } = err;
-    code = statusCode;
-  }
-
-  return { err, statusCode: code };
 };
 
 export default ErrorPage;
