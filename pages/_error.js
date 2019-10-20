725d240d1873a08e8682404cce6a7019d982637a
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2019-10-20 21:44:09 +0200

    #1: Slightly fancier error page

--- pages/_error.js
+++ pages/_error.js
@@ -5,8 +5,21 @@ const ErrorPage = props => {
   const { statusCode } = props;
 
   return (
-    <div>
-      <p>Error {statusCode}</p>
+    <div className="splash-page flex flex-col flex-grow justify-center items-center">
+      <p className="mx-8 text-center text-3xl text-mariner font-bold">
+        {statusCode}
+      </p>
+
+      <p className="mx-8 mt-8 text-center text-xl text-gray-700">
+        Well, well, well. What do we have here? Someone trying to sneak into
+        pages not yet released, huh.
+      </p>
+
+      <p className="mx-8 text-center text-xl text-gray-700">
+        Get outta here, you, and come back another time.
+      </p>
+
+      <p className="mx-8 mt-8 text-center text-xl text-gray-700">Thanks.</p>
     </div>
   );
 };
