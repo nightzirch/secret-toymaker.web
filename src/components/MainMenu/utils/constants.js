b3c20f7dc95062c8b8e45521f284359c368e9642
Rhayven <codingforllamas@gmail.com>
2019-11-18 21:36:00 -0500

    Nav updates
    -Update nav routes
    -Some linting to satisfy ESLint
    -Slight updates to nav menu for accessibility

--- src/components/MainMenu/utils/constants.js
+++ src/components/MainMenu/utils/constants.js
@@ -1,3 +1,9 @@
-import * as ROUTES from "routes";
+import * as ROUTES from "../../../routes";
 
-export const NAVIGATION_ITEMS = [{ title: "Home", url: ROUTES.FRONTPAGE }];
+export const NAVIGATION_ITEMS = [{ title: "Home", url: ROUTES.FRONTPAGE }, {title: "Faq", url: ROUTES.FAQ }, { title: "Sign In" , url: ROUTES.SIGNIN},{title: "Sign Up", url: ROUTES.SIGNUP }, /*  { title: "Profile", url: ROUTES.PROFILE }, */ {title: "Gifts", url: ROUTES.GIFTS }];
+
+/*
+  TODO: Add Authentication so you can't navigate to /profile if you're not logged in.
+  TODO: Add check to make sure you're logged in before showing profile.
+  TODO: Remove comment block so profile shows.
+*/
