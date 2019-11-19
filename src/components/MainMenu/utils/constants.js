4e92c1ac001464c499d36bb72edc236c33e65025
Unknown <Christian.Grimsgaard@knowit.no>
2019-11-19 09:02:54 +0100

    Formatted code

--- src/components/MainMenu/utils/constants.js
+++ src/components/MainMenu/utils/constants.js
@@ -1,6 +1,13 @@
 import * as ROUTES from "../../../routes";
 
-export const NAVIGATION_ITEMS = [{ title: "Home", url: ROUTES.FRONTPAGE }, {title: "Faq", url: ROUTES.FAQ }, { title: "Sign In" , url: ROUTES.SIGNIN},{title: "Sign Up", url: ROUTES.SIGNUP }, /*  { title: "Profile", url: ROUTES.PROFILE }, */ {title: "Gifts", url: ROUTES.GIFTS }];
+export const NAVIGATION_ITEMS = [
+  { title: "Home", url: ROUTES.FRONTPAGE },
+  { title: "Faq", url: ROUTES.FAQ },
+  { title: "Sign In", url: ROUTES.SIGNIN },
+  { title: "Sign Up", url: ROUTES.SIGNUP },
+  /*  { title: "Profile", url: ROUTES.PROFILE }, */
+  { title: "Gifts", url: ROUTES.GIFTS }
+];
 
 /*
   TODO: Add Authentication so you can't navigate to /profile if you're not logged in.
