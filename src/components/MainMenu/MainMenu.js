599ffaeb32ee0fff70ff3ee43712dca79bd9f093
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2019-12-01 00:23:50 +0100

    Placing the main menu correctly

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,4 +1,6 @@
 import classnames from "classnames";
+import Hamburger from "components/Hamburger";
+import Section from "components/Section";
 import { Link } from "react-router-dom";
 import React, { useGlobal } from "reactn";
 import "./MainMenu.scss";
@@ -7,7 +9,7 @@ import { NAVIGATION_ITEMS } from "./utils/constants";
 const MainMenu = props => {
   const [isMenuOpen, setIsMenuOpen] = useGlobal("isMenuOpen");
 
-  const handleBackdropClick = () => {
+  const handleMenuCloseClick = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
@@ -20,14 +22,29 @@ const MainMenu = props => {
   };
 
   return (
-    <>
+    <div className={classnames("main-menu", { "main-menu--open": isMenuOpen })}>
+      <Section>
+        <nav
+          aria-labelledby="main-navigation"
+          role="navigation"
+          className="main-menu__nav"
+        >
+          <div className="main-menu__hamburger-wrapper">
+            <Hamburger onClick={handleMenuCloseClick} isMenuOpen={isMenuOpen} />
+          </div>
+
+          <div className="main-menu__items-wrapper">
+            {renderNavigationItems()}
+          </div>
+        </nav>
+      </Section>
+
       <div
-        className={classnames("main-menu", { "main-menu--open": isMenuOpen })}
-      >
-        <nav className="main-menu__nav">{renderNavigationItems()}</nav>
-      </div>
-      <div className="main-menu-backdrop" role="presentation" onClick={handleBackdropClick} />
-    </>
+        className="main-menu__backdrop"
+        role="presentation"
+        onClick={handleMenuCloseClick}
+      />
+    </div>
   );
 };
 
