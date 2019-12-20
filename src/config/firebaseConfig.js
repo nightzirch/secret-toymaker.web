865e8c6851dd028c10d33145f2af453e36f9b35c
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2019-12-20 20:50:01 +0100

    Upgraded Firebase to include analytics

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -4,5 +4,7 @@ module.exports = {
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
