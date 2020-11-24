449e7aa0cc1a05f0ba2cdf415e8fcef75a35c5c2
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2020-11-24 09:56:30 +0100

    Rendering participations on profile page

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,21 +1,57 @@
 import Alerts from "@/components/Alerts";
 import ApiTokenForm from "@/components/ApiTokenForm";
 import { ResetPasswordButton, SignoutButton } from "@/components/Button";
+import EventCard from "@/components/EventCard";
 import { Grid, GridItem } from "@/components/Grid";
 import NotificationsForm from "@/components/NotificationsForm";
 import ProfileForm from "@/components/ProfileForm";
 import ProfilePicture from "@/components/ProfilePicture";
 import Section from "@/components/Section";
 import { PageHeader, Title } from "@/components/Typography";
+import { dispatchWithLoading } from "@/utils/loading";
 import { validateAuthWithRedirect } from "@/utils/redirect";
+import ActionTypes from "@/utils/types/ActionTypes";
 import AlertLocationTypes from "@/utils/types/AlertLocationTypes";
+import t from "prop-types";
+import { useEffect } from "react";
+import { withGlobal } from "reactn";
 
 const ProfilePage = (props) => {
+  const { events, loading, participations, user } = props;
+
+  useEffect(() => {
+    if (user) dispatchWithLoading(ActionTypes.GET_PARTICIPATION_STATUS);
+  }, [user]);
+
   const renderProfilePicture = () => (
     <>
       <ProfilePicture />
     </>
   );
+
+  const renderParticipations = () => {
+    if (!participations) return null;
+    const participationYears = participations.map((p) => p.year);
+
+    return (
+      participationYears.length > 0 && (
+        <>
+          <Title level="secondary">Participations</Title>
+
+          <Grid columns={4}>
+            {Object.values(events)
+              .filter((event) => participationYears.includes(event.year))
+              .sort((a, b) => (a.year > b.year ? -1 : 1))
+              .map((event) => (
+                <GridItem span={4} spanMobile={12} key={event.year}>
+                  <EventCard event={event} />
+                </GridItem>
+              ))}
+          </Grid>
+        </>
+      )
+    );
+  };
 
   const renderDangerZone = () => (
     <>
@@ -41,6 +77,8 @@ const ProfilePage = (props) => {
           <GridItem span={4}>
             <Title level="secondary">Avatar</Title>
             {renderProfilePicture()}
+
+            {renderParticipations()}
           </GridItem>
 
           <GridItem span={8}>
@@ -63,7 +101,21 @@ const ProfilePage = (props) => {
   );
 };
 
+ProfilePage.propTypes = {
+  events: t.object,
+  loading: t.object,
+  participations: t.array,
+  user: t.object,
+};
+
+const mapGlobalToProps = (global) => ({
+  events: global.events,
+  loading: global.loading,
+  participations: global.participations,
+  user: global.user,
+});
+
 export const getServerSideProps = async (ctx) =>
   await validateAuthWithRedirect(ctx);
 
-export default ProfilePage;
+export default withGlobal(mapGlobalToProps)(ProfilePage);
